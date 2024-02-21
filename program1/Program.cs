d581b3f9c0382b19ab17dd07f7241386d527f92d
ARUP GOPE <94756772+arup1221@users.noreply.github.com>
2024-02-21 09:53:15 +0530

    Add files via upload

---
+++ program1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class ArmstrongNumbers
+{
+    static int CountDigits(int num)
+    {
+        int count  =0;
+        while(num > 0)
+        {
+            num /= 10;
+            count++;
+        }
+        return count;
+    }
+
+    static bool isArmStrong(int num)
+    {
+        int n = num;
+        int cdigit = CountDigits(num);
+        int sum =0;
+        while(num > 0)
+        {
+            int digit = num%10;
+            sum += (int)Math.Pow(digit,cdigit);
+            num /=10;
+        }
+        return n == sum;
+    }
+    static void Main()
+    {
+        Console.Write("Armstrong number between 1 and 1000");
+        for(int i =0;i<1000;i++)
+        {
+            if(isArmStrong(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ program1/program2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Drawing;
+class Substring
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your String");
+        String s = Convert.ToString(Console.Read());
+        Console.WriteLine("All subString are: ");
+        int size = s.Length;
+        for(int i =0;i<size;i++)
+        {
+            for(int j =i;j<size;j++)
+            {
+                String ans = s.Substring(i,j-i+1);
+                Console.WriteLine(ans);
+            }
+        }
+
+    }
+}
